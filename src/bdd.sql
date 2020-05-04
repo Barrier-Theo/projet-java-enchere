cf9b455043740826c2ecd62313c9ddbeaf57d094
Théo Barrier <theo.barrier@hotmail.fr>
2020-05-04 09:12:35 +0200

    modification bdd ajout du boolean isDelete

--- src/bdd.sql
+++ src/bdd.sql
@@ -104,4 +104,4 @@ SELECT * FROM UTILISATEURS where pseudo = 'tbarrier' and mot_de_passe = '123mdp'
 
 ALTER TABLE UTILISATEURS ADD isDelete BIT NOT NULL DEFAULT 0;
 
-UPDATE UTILISATEURS SET isDelete = 0 WHERE pseudo = "tbarrierd";
+UPDATE UTILISATEURS SET isDelete = 0 WHERE pseudo = 'tbarrierd';
