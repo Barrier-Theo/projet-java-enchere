69d917703959f247721014a2aa822e9e687bd48e
Théo Barrier <theo.barrier@hotmail.fr>
2020-05-04 16:26:57 +0200

    WIP enchre detais

---
+++ src/servlet/ServletEnchere.java
@@ -0,0 +1,37 @@
+package servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import bll.ArticlesVendusManager;
+
+/**
+ * Servlet implementation class ServletEnchere
+ */
+@WebServlet("/ServletEnchere")
+public class ServletEnchere extends HttpServlet {
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		Integer id = Integer.parseInt(request.getParameter("id"));
+		ArticlesVendusManager articleManager = new ArticlesVendusManager();
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
